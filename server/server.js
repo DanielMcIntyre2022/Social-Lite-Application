85443270562ea3e16d5c3000c5228c5452196ac3
Daniel <daniel-mcintyre@hotmail.com>
2022-07-20 15:45:31 -0400

    updates to backend

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express'); //needed to launch server
 const cors = require('cors'); //needed to disable sendgrid security
 const app = express(); //alias from the express function
 app.use(cors);
-const axios = require('axios');
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -13,21 +12,9 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 
-  server.get("/", (req, res) => {
-  const configEmail = {
-    method: 'GET',
-    url: 'https://localhost:4000'
-  };
-  axios(configEmail)
-    .then((response) => {
-      console.log(response)
-      res.json(response);
-      console.log(response);
-  })
-});
-
-});
-
+  server.post("/", (req, res) => {
+    const emailInfo = req.body.emailUserInput;
+  
 // sendgrid details //
 
 require('dotenv').config();
@@ -35,13 +22,13 @@ const sgMail = require('@sendgrid/mail');
 const apikey = process.env.SENDGRID_API_KEY
 sgMail.setApiKey(apikey);
 const msg = {
-  to: 'daniel-mcintyre@hotmail.com',
+  to: emailInfo,
   from: 'socialliteeventservices@gmail.com', 
   subject: 'Sending with Twilio SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
-
+  
 // email sending logic //
 
 //ES6
@@ -66,6 +53,10 @@ sgMail
     }
   }
 })();
+    
+    })
+  
+  });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
