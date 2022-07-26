c9273729265007a56dfff2bc343bd422c627d014
Sejuti Banik <banik.sejuti25@gmail.com>
2022-07-25 23:07:15 -0400

    Header.js has Homepage settings

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,37 @@
-import "../StyleComponents/Header.css";
-import { useNavigate } from 'react-router-dom';
-import headerImage from '../assets/Socialite-Logo.jpg';
+import React from 'react'
+import logo from '../images/Logo.jpg'
+import Social from '../images/Socialite-Landing-Main.jpg'
+import "../StyleComponents/Header.css"
+import { Routes, Route, useNavigate } from 'react-router-dom';
 
-function Header() {
+function HomePage() {
 
-    const Navigate = useNavigate(); 
+  
+  const navigate = useNavigate();
+  const navigateCreateEvent = () => {
+    navigate('/CreateEvent');
+  };
+  return (
+    <div className='full-body'>
+      <div className='home-container'>
+        <img className='soc_logo' src={logo} alt="Socialite logo"/>
+      </div>
+      <img className='home_jpg' src={Social} alt="Friends Main" />
+      
+      <div className='text-style'>
+        <span><p2 className="normal">Easily</p2><p2 className="bold"> create, view & </p2></span><br/>
+        <span><p2 className="bold"> share </p2> <p2 className="normal"> events with</p2></span><br/>
+        <span><p2 className="normal">your friends!</p2></span>
+      </div>
+      <div className='box'>
+        <button className="event-create-button" type="button" onClick={navigateCreateEvent}>Create an Event</button>
+        
+        <Routes>
+          <Route path="/CreateEvent" />
+        </Routes>
+      </div>
+    </div>
+  )
+}
 
-    const returnHome = () => {
-        Navigate('/CreateEvent')
-    }
-
-    return (
-        <div className="header-container">
-        <header>
-                <img src={headerImage} alt="social-lite logo" onClick={returnHome}></img>
-            </header>
-        </div>
-)}
-
-export default Header;
+export default HomePage
